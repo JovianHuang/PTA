34538233f935498bfc03302817d076e607c76574
Jovian <qianwei_king@163.com>
2018-05-22 08:24:26 +0800

    update_1805220824

---
+++ 7-60.c
@@ -0,0 +1,25 @@
+/*
+* =========================================================================== *
+* @Author:		JovianHuang
+* @Filename:	7-60.c
+* @Description:	A question which numbered 7-60 in PTA. This question requires
+				that the program be written to balabala
+* @Version:		1.0.0.180517_alpha
+* =========================================================================== *
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define LEN 80
+
+void Swap()
+
+int main(int argc, char const *argv[])
+{
+	char str[LEN];
+	fgets(str, LEN, stdin);
+
+
+
+	return 0;
+}
